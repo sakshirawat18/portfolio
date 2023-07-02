a2a51e463b1231476b77dd22e4c9ed81fa15971c
Sakshi Rawat <sakshirawat@Sakshis-MacBook-Air.local>
2023-07-02 22:00:46 +0530

    project-setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Hero/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      project
+    </div>
+  )
+}
+
+export default Projects
